6df69e7f800e605e0f6ebc91cb52764b5466e6a3
Tyrone Josh Tang-o <155046372+tyronejoshtang-o@users.noreply.github.com>
2024-01-02 18:21:54 +0800

    Update magic8ball.java

--- magic8ball.java
+++ magic8ball.java
@@ -1,42 +1,47 @@
-package magiceightball;
+package magic8ball;
+    import java.util.Scanner;
+    import java.util.Random;
 
-import java.util.Scanner;
-import java.util.Random;
-
-public class Magiceightball {
-
+public class Magic8ball {
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
         Random randomInt = new Random();
         
+        System.out.println("Welcome to Oracle of Fate. Believe or not,  this magic ball knows all the answers.");
+      
+        System.out.print("Input your name: ");
+        String name = scan.nextLine();
+        name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+        
+        
         String question;   
         int random = randomInt.nextInt(20)+1;
         String answer;
         
-        System.out.println("Ask your question: ");
+        System.out.println("Hello " + name + "! Ask your question: ");
         question = scan.nextLine();  
         
         if (random  == 1) {
-            answer = "IT IS CERTAIN";
+            answer = "\033[1;32mIT IS CERTAIN\033[0m";
         } else if (random == 2) {
-            answer = "IT IS DECIDEDLY SO";
+            answer = "\033[1;32mIT IS DECIDEDLY SO\033[0m";
         } else if (random == 3) {
-            answer = "WITHOUT A DOUBT";
+            answer = "\033[1;32mWITHOUT A DOUBT\033[0m";
         } else if (random == 4) {
-            answer = "YES, DEFINITELY";
+            answer = "\033[1;32mYES, DEFINITELY\033[0m";
         } else if (random == 5) {
-            answer = "YOU MAY RELY ON IT";
+            answer = "\033[1;32mYOU MAY RELY ON IT\033[0m";
         } else if (random == 6) {
-            answer = "AS I SEE IT, YES";
+            answer = "\033[1;32mAS I SEE IT, YES\033[0m";
         } else if (random == 7) {
-            answer = "MOST LIKELY";
+            answer = "\033[1;32mMOST LIKELY\033[0m";
         } else if (random == 8) {
-            answer = "OUTLOOK GOOD";
+            answer = "\033[1;32mOUTLOOK GOOD\033[0m";
         } else if (random == 9) {
-            answer = "YES";
+            answer = "\033[1;32mYES\033[0m";
         } else if (random == 10) {
-            answer = "SIGNS POINT TO YES";
+            answer = "\033[1;32mSIGNS POINT TO YES\033[0m";
         } else if (random == 11) {
             answer = "REPLY HAZY TRY AGAIN";
         } else if (random == 12) {
@@ -48,15 +53,15 @@ public class Magiceightball {
         } else if (random == 15) {
             answer = "CONCENTRATE AND ASK AGAIN";
         } else if (random == 16) {
-            answer = "DON'T COUNT ON IT";
+            answer = "\033[1;31mDON'T COUNT ON IT\033[0m";
         } else if (random == 17) {
-            answer = "MY REPLY IS NO";
+            answer = "\033[1;31mMY REPLY IS NO\033[0m";
         } else if (random == 18) {
-            answer = "MY SOURCES SAY NO";
+            answer = "\033[1;31mMY SOURCES SAY NO\033[0m";
         } else if (random == 19) {
-            answer = "OUTLOOK NOT SO GOOD";
+            answer = "\033[1;31mOUTLOOK NOT SO GOOD\033[0m";
         } else {
-            answer = "VERY DOUBTFUL";
+            answer = "\033[1;31mVERY DOUBTFUL\033[0m";
     }
         System.out.println(answer);
        
